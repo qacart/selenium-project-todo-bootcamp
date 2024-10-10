74f5b1bb0b15bb42ac8dfb6295e9f72ae91f72c7
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-10 22:44:19 +0200

    build the register method

--- src/test/java/com/qacart/todo/pages/RegisterPage.java
+++ src/test/java/com/qacart/todo/pages/RegisterPage.java
@@ -1,13 +1,54 @@
 package com.qacart.todo.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.time.Instant;
 
 public class RegisterPage {
-    private By firstNameLocator = By.cssSelector("[data-testid=first-name]");
-    private By lastNameLocator = By.cssSelector("[data-testid=last-name]");
-    private By emailInputLocator = By.cssSelector("[data-testid=email]");
-    private By passwordInputLocator = By.cssSelector("[data-testid=password]");
-    private By confirmPasswordLocator = By.cssSelector("[data-testid=confirm-password]");
-    private By submitButtonLocator = By.cssSelector("[data-testid=submit]");
+    private WebDriver driver;
+    // Locators
+    private final By FIRST_NAME_LOCATOR = By.cssSelector("[data-testid=first-name]");
+    private final By LAST_NAME_LOCATOR = By.cssSelector("[data-testid=last-name]");
+    private final By EMAIL_INPUT_LOCATOR = By.cssSelector("[data-testid=email]");
+    private final By PASSWORD_INPUT_LOCATOR = By.cssSelector("[data-testid=password]");
+    private final By CONFIRM_PASSWORD_LOCATOR = By.cssSelector("[data-testid=confirm-password]");
+    private final By SUBMIT_BUTTON_LOCATOR = By.cssSelector("[data-testid=submit]");
+
+    // Constructor
+    public RegisterPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    // Methods
+    public void register() {
+        // Fill the first name
+        WebElement firstNameInput = driver.findElement(FIRST_NAME_LOCATOR);
+        firstNameInput.sendKeys("Hatem");
+
+        // Fill the last name
+        WebElement lastNameInput = driver.findElement(LAST_NAME_LOCATOR);
+        lastNameInput.sendKeys("Hatamleh");
+
+        // Fill the email
+        long currentTimeStamp = Instant.now().toEpochMilli();
+        String email = "auto_test" + currentTimeStamp + "@example.com";
+        WebElement emailInput = driver.findElement(EMAIL_INPUT_LOCATOR);
+        emailInput.sendKeys(email);
+
+        // Fill the password
+        WebElement passwordInput = driver.findElement(PASSWORD_INPUT_LOCATOR);
+        passwordInput.sendKeys("Test1234");
+
+        // Fill the confirm password input
+        WebElement confirmPasswordInput = driver.findElement(CONFIRM_PASSWORD_LOCATOR);
+        confirmPasswordInput.sendKeys("Test1234");
+
+        // Click on the signup button
+        WebElement submitButton = driver.findElement(SUBMIT_BUTTON_LOCATOR);
+        submitButton.click();
+    }
 
 }
--- src/test/java/com/qacart/todo/testcases/RegisterTest.java
+++ src/test/java/com/qacart/todo/testcases/RegisterTest.java
@@ -1,6 +1,7 @@
 package com.qacart.todo.testcases;
 
 import com.qacart.todo.base.BaseTest;
+import com.qacart.todo.pages.RegisterPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -13,36 +14,12 @@ public class RegisterTest extends BaseTest {
     void should_be_able_to_register() {
         driver.get("https://todo.qacart.com/signup");
 
-        // Fill the first name
-        WebElement firstNameInput = driver.findElement(By.cssSelector("[data-testid=first-name]"));
-        firstNameInput.sendKeys("Hatem");
-
-        // Fill the last name
-        WebElement lastNameInput = driver.findElement(By.cssSelector("[data-testid=last-name]"));
-        lastNameInput.sendKeys("Hatamleh");
-
-        // Fill the email
-        long currentTimeStamp = Instant.now().toEpochMilli();
-        String email = "auto_test" + currentTimeStamp + "@example.com";
-        WebElement emailInput = driver.findElement(By.cssSelector("[data-testid=email]"));
-        emailInput.sendKeys(email);
-
-        // Fill the password
-        WebElement passwordInput = driver.findElement(By.cssSelector("[data-testid=password]"));
-        passwordInput.sendKeys("Test1234");
-
-        // Fill the confirm password input
-        WebElement confirmPasswordInput = driver.findElement(By.cssSelector("[data-testid=confirm-password]"));
-        confirmPasswordInput.sendKeys("Test1234");
-
-        // Click on the signup button
-        WebElement submitButton = driver.findElement(By.cssSelector("[data-testid=submit]"));
-        submitButton.click();
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.register();
 
         // Assert the welcome message is displayed
         WebElement welcomeMessage = driver.findElement(By.cssSelector("[data-testid=welcome]"));
         Assert.assertTrue(welcomeMessage.isDisplayed());
-
 
     }
 }
