7a3c7888ed141416282f4b016ce1dbfe9b9783e4
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-10 13:45:31 +0200

    make the email dynamic

--- src/test/java/com/qacart/todo/testcases/RegisterTest.java
+++ src/test/java/com/qacart/todo/testcases/RegisterTest.java
@@ -4,16 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.time.Instant;
 
 public class RegisterTest {
 
     @Test
     void should_be_able_to_register() {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://todo.qacart.com/signup");
@@ -27,8 +32,10 @@ public class RegisterTest {
         lastNameInput.sendKeys("Hatamleh");
 
         // Fill the email
+        long currentTimeStamp = Instant.now().toEpochMilli();
+        String email = "auto_test" + currentTimeStamp + "@example.com";
         WebElement emailInput = driver.findElement(By.cssSelector("[data-testid=email]"));
-        emailInput.sendKeys("auto_test3@example.com");
+        emailInput.sendKeys(email);
 
         // Fill the password
         WebElement passwordInput = driver.findElement(By.cssSelector("[data-testid=password]"));
