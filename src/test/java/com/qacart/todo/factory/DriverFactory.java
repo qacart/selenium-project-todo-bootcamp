af655fc7699fde444c9ea33806ad580c7b28e611
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-11 23:24:50 +0200

    Build the driver factory using switch statement

--- src/test/java/com/qacart/todo/factory/DriverFactory.java
+++ src/test/java/com/qacart/todo/factory/DriverFactory.java
@@ -9,28 +9,25 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import java.time.Duration;
 
 public class DriverFactory {
-    String browserName = System.getProperty("browserName" , "Chrome");
+    String browserName = System.getProperty("browserName", "Chrome");
 
     public WebDriver initializeDriver() {
         WebDriver driver;
-        if(browserName.equalsIgnoreCase("CHROME")) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            driver = new ChromeDriver(options);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-            return driver;
-        } else if (browserName.equalsIgnoreCase("FIREFOX")) {
-            FirefoxOptions options = new FirefoxOptions();
-            options.addArguments("--headless");
-            driver = new FirefoxDriver(options);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-            return driver;
-        } else {
-            throw new IllegalArgumentException("Browser is not supported");
+        switch (browserName.toUpperCase()) {
+            case "CHROME" -> {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
+            }
+            case "FIREFOX" -> {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                driver = new FirefoxDriver(options);
+            }
+            default -> throw new IllegalArgumentException("Browser is not supported");
         }
-
-
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        return driver;
     }
 }
