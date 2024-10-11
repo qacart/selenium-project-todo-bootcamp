1a6b6b1acf664814a4823fa61e8145095cd173be
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-11 23:27:33 +0200

    Remove unused imports

--- src/test/java/com/qacart/todo/base/BaseTest.java
+++ src/test/java/com/qacart/todo/base/BaseTest.java
@@ -2,12 +2,8 @@ package com.qacart.todo.base;
 
 import com.qacart.todo.factory.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.time.Duration;
 
 public class BaseTest {
 
