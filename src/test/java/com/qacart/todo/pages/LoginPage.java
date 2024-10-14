1208d39434c243d4dc33e605855937040d283871
Hatem Hatamleh <hatem.hatamleh@gmail.com>
2024-10-14 12:24:37 +0200

    Add steps to allure report and add aspectJ

--- src/test/java/com/qacart/todo/pages/LoginPage.java
+++ src/test/java/com/qacart/todo/pages/LoginPage.java
@@ -2,6 +2,7 @@ package com.qacart.todo.pages;
 
 import com.qacart.todo.models.User;
 import com.qacart.todo.utils.ConfigUtil;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -12,10 +13,12 @@ public class LoginPage {
     private final By SUBMIT_BUTTON_LOCATOR = By.cssSelector("[data-testid=submit]");
 
 
+    @Step
     public void load(WebDriver driver) {
         driver.get(ConfigUtil.getInstance().getBaseUrl() + "/login");
     }
 
+    @Step
     public void login(WebDriver driver, User user) {
         driver.findElement(EMAIL_INPUT_LOCATOR).sendKeys(user.getEmail());
         driver.findElement(PASSWORD_INPUT_LOCATOR).sendKeys(user.getPassword());
--- src/test/java/com/qacart/todo/pages/NewTodoPage.java
+++ src/test/java/com/qacart/todo/pages/NewTodoPage.java
@@ -2,6 +2,7 @@ package com.qacart.todo.pages;
 
 import com.qacart.todo.models.Todo;
 import com.qacart.todo.utils.ConfigUtil;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -10,10 +11,12 @@ public class NewTodoPage {
     private final By NEW_TASK_INPUT_LOCATOR = By.cssSelector("[data-testid=new-todo]");
     private final By SUBMIT_NEW_TASK_LOCATOR = By.cssSelector("[data-testid=submit-newTask]");
 
+    @Step
     public void load(WebDriver driver) {
         driver.get(ConfigUtil.getInstance().getBaseUrl() + "todo/new");
     }
 
+    @Step
     public void createNewTodo(WebDriver driver, Todo todo) {
         driver.findElement(NEW_TASK_INPUT_LOCATOR).sendKeys(todo.getName());
         driver.findElement(SUBMIT_NEW_TASK_LOCATOR).click();
